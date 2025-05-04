9767337384e429349cc09dbcc5f1ecd2e5168fd4
afarenziya <afarenziya@gmail.com>
2025-05-04 14:20:34 +0530

    Initial commit

--- client/src/pages/expenses-page.tsx
+++ client/src/pages/expenses-page.tsx
@@ -4,16 +4,28 @@ import { DateFilterComponent } from "@/components/filters/date-filter";
 import { ExpenseTable } from "@/components/expenses/expense-table";
 import { DateFilter } from "@shared/schema";
 import { getDateFilter } from "@/lib/date-utils";
+import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
+import { ExpenseForm } from "@/components/expenses/expense-form";
 
 export default function ExpensesPage() {
   const [dateFilter, setDateFilter] = useState<DateFilter>(
     getDateFilter("this_month")
   );
+  const [isAddExpenseOpen, setIsAddExpenseOpen] = useState(false);
 
   const handleDateFilterChange = (filter: DateFilter) => {
     setDateFilter(filter);
   };
-  
+
+  const handleAddExpense = () => {
+    setIsAddExpenseOpen(true);
+  };
+
+  const handleCloseAddExpense = () => {
+    setIsAddExpenseOpen(false);
+  };
+
   return (
     <Layout
       title="Expenses"
@@ -21,7 +33,17 @@ export default function ExpensesPage() {
     >
       {/* Date Filter */}
       <DateFilterComponent onFilterChange={handleDateFilterChange} />
-      
+
+      {/* Add New Expense Button */}
+      <div className="my-4">
+        <Button onClick={handleAddExpense}>Add New Expense</Button>
+      </div>
+
+      {/* Add Expense Dialog */}
+      <Dialog isOpen={isAddExpenseOpen} onClose={handleCloseAddExpense}>
+        <ExpenseForm onClose={handleCloseAddExpense} />
+      </Dialog>
+
       {/* Expenses Table */}
       <ExpenseTable dateFilter={dateFilter} />
     </Layout>
